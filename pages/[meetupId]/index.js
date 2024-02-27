5caab1928435ea7ed176e1781c813cff263d8ba9
michalrudy9 <michallrudy9@gmail.com>
2024-02-27 12:46:00 +0100

    Provided SSG for meetup detail

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,38 @@ function MeetupDetailsPage() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetailsPage;
