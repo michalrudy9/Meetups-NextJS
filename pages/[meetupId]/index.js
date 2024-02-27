65e13c84b305647804f061f3d39230531bb60d4f
michalrudy9 <michallrudy9@gmail.com>
2024-02-27 11:04:24 +0100

    Finished preparing skeleton

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1600px-Stadtbild_M%C3%BCnchen.jpg?20130611211153"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetailsPage;
